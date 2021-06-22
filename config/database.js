dfa539783d297d93b3c3201a586b4cc26ca65f10
erickmgongo <erickemmanuel661@gmail.com>
2021-06-22 09:23:54 +0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,38 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'og'),
+//         username: env('DATABASE_USERNAME', 'postgres'),
+//         password: env('DATABASE_PASSWORD', 'postgres'),
+//         schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+//         ssl: {
+//           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//         },
+//       },
+//       options: {},
+//     },
+//   },
+// });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9f57f058-ce38-4335-875b-f841d3b22af2'
+};
